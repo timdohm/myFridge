f1f49a8af475ba5b9583d117a30bad7658c5c077
Alexandra Kauffman <alexandrakauffman@Alexandras-MacBook-Pro.local>
2017-04-30 17:04:02 -0500

    expiration buton

--- myFridge/www/js/controllers.js
+++ myFridge/www/js/controllers.js
@@ -161,7 +161,7 @@ angular.module('starter.controllers', [])
   $scope.checkExp = function() {
     if ($scope.todaysDate >= $scope.itemInput.expDate) {
       console.log("expired!")
-      $scope.isColor = "red";
+      $scope.isColor = "assertive";
     }
   }
 })
@@ -226,8 +226,13 @@ angular.module('starter.controllers', [])
   $scope.goRecipe = function(recipe) {
     $ionicViewSwitcher.nextDirection('forward');
     $state.go('recipeDisp', {recipe: recipe});
-  };
 
+  };
+  var listArray = [];
+  for (var i = 0; i < $scope.list.length; i++) {
+    listArray.push($scope.list.indexOf($scope.list[i].item))
+  }
+  console.log(listArray)
 })
 
 .controller('RecipeDispCtrl', function($scope, $state, $stateParams, $ionicModal, $ionicHistory, APIController, FindByIngredientsModel){
